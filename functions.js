cda079906080d0220f9c97abdf8c0ac9751acfe8
David Hernandez <davyd2h@gmail.com>
2024-05-28 15:54:05 -0500

    función concatenar implementando for

--- functions.js
+++ functions.js
@@ -3,7 +3,10 @@ let textContactenado = '';
 let nombres = ["Alice", "Bob", "Charlie", "David"];
 
 for(let i = 0; i < nombres.length; i++) {
-    textContactenado += nombres[i] + ', '
+    textContactenado += nombres[i]
+    if(i < nombres.length - 1) {
+        textContactenado += ', '
+    }
 }
 
 console.log(textContactenado)
