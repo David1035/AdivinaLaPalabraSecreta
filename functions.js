499b4436793f0e0c16e713a380c122dcbfe807d6
David Hernandez <davyd2h@gmail.com>
2024-05-30 11:55:48 -0500

    ejercicio ciclo while, clase

--- functions.js
+++ functions.js
@@ -1,27 +1,14 @@
-const calificaciones = {
-    Alice: 85,
-    Bob: 92,
-    Charlie: 87,
-    David: 90
-};
-
-let sumaCalificaciones = 0;
-let numeroEstudiantes = 0;
-let calificacionMaxima = 0;
-let mejorEstudiante = "";
-
-// Iterar sobre el objeto calificaciones usando for...in
-for (let estudiante in calificaciones) {
-    sumaCalificaciones += calificaciones[estudiante];
-    numeroEstudiantes += 1;
-    if (calificaciones[estudiante] > calificacionMaxima) {
-        calificacionMaxima = calificaciones[estudiante];
-        mejorEstudiante = estudiante;
-    }
+/*
+while(condición) {
+    // Código a ejecutar
 }
 
-// Calcular la calificación promedio
-let calificacionPromedio = sumaCalificaciones / numeroEstudiantes;
+*/
 
-// Imprimir el estudiante con la calificación más alta y la calificación promedio
-console.log(`El mejor estudiante es ${mejorEstudiante} y tiene una calificación de ${calificacionMaxima}, siendo la calificación más alta. La nota promedio de los estudiantes es ${calificacionPromedio}.`);
+let contador = 0;
+let maximo = 10;
+
+while(contador < maximo) {
+    console.log(contador);
+    contador++;
+}
