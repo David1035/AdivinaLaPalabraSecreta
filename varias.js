35b721bed891d166c38f1642b31b25af487e06e4
David Hernandez <davyd2h@gmail.com>
2024-06-03 18:53:54 -0500

    se crea

---
+++ varias.js
@@ -0,0 +1,9 @@
+// en este programa puedes verificar si está dentro de los ganadores de una rifa, ingresando el nombre o número de ticket. 
+
+const winningParticipants = [
+    { id: 8, name: 'Juan', ticket: 8 },
+    { id: 15, name: 'Pedro', ticket: 15 },
+    { id: 21, name: 'Maria', ticket: 21 },
+]
+
+
