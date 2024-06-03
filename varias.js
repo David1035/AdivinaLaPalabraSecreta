7b6a145cf17409e5321a1fc559b9f063df4ccabe
David Hernandez <davyd2h@gmail.com>
2024-06-03 18:45:28 -0500

    se crean dos rectos pendientes por realiar

---
+++ varias.js
@@ -0,0 +1,6 @@
+const nombres1 = ['Alice', 'Bob', 'Charlie'];
+const nombres2 = ['David', 'Eve', 'Frank'];
+const nombres3 = ['luis', 'luisa', 'julio']
+const nombresJuntos = nombres1.concat(nombres2).concat(nombres3)
+
+console.log(nombresJuntos)
