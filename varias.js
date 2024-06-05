49af9f613cceb8cec80b359003622074733c9fef
David Hernandez <davyd2h@gmail.com>
2024-06-05 08:55:34 -0500

    se crea juego funcional, e iterando completamente

--- varias.js
+++ varias.js
@@ -1,9 +1,20 @@
 //Ejercise
 function tournnamentWinner (competitions, result){
+    const scores = {};
+    let winner = '';
+
     for(let i = 0; i < competitions.length; i++){
         const [home, away] = competitions[i];
         const winningTeam = result[i] === 0 ? away : home
+
+        scores[winningTeam] = (scores[winningTeam] || 0) + 3
+
+        if(!winner || scores[winningTeam] > scores[winner]){
+            winner = winningTeam
+        }
+
     }
+    return winner
 }
 
 const competitions = [
@@ -12,6 +23,6 @@ const competitions = [
     ['Python', 'JavasScript']
 ]
 
-const result = [0,0,1]
+const result = [1,0,0]
 
 console.log(tournnamentWinner (competitions, result))
