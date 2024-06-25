f71b61174696d52c0d0aa7abb72434f6c9b0d178
David Hernandez <davyd2h@gmail.com>
2024-06-25 08:50:29 -0500

    modificacion y trabajo

--- varias.js
+++ varias.js
@@ -1,20 +1,38 @@
-
-
-/*const person = {
-    name: 'David',
-    lastName: 'Hernandez',
-    age: 30,
-}*/
-
-// función constructora, se inicia con mayúscula
-function Person(name,  lastName, age){
-    this.name = name;
-    this.lastName = lastName;
-    this.age = age;
+class Animal {
+    constructor(name, tipo) {
+        this.name = name;
+        this.tipo = tipo;
+    }
+    emitirSonido(){
+        console.log('animal ')
+    }
 }
 
-//Instancias que vengan de un objeto 
-const person1 = new Person('David', 'Hernandez', 30)
-const person2 = new Person('David', 'Hernandez', 30)
+// se está haciendo una clase nueva, no se está utilizando la herencia.
+class Perro extends Animal{ // extends para traer el constructor Animal
+    constructor(name, tipo, raza) {
+        super(name, tipo); // se utilizar super para utilizar class base
+        this.raza = raza;
+    }
+    emitirSonido() {
+        console.log('guau');
+    }
+    correr() {
+        console.log(`${this.name} corre feliz`)
+    }
+}
 
+const perro1 = new Perro ('Zeus', 'margi', 'biggol')
 
+perro1.correr();
+
+//clase Prototipos en la práctica
+
+perro1.nuevoMetodo = function () {
+    console.log('Este es un método')
+} // estaría agregando un método solo a la instancia, aquí no toma herencia. Se tiene que inyectar al prototipo que se genera
+// la forma de hacerlo es
+
+perro1.prototype.segundoMetodo = function () {
+    console.log('nuevo método')
+}
